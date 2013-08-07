813be86b6cf249f745b5c467f7a90d7dae14f61b
Corminboeuf Thomas <tcormin@bluewin.ch>
2013-08-07 22:45:53 +0200

    Cleaning

--- benevolat-root/build.cmd
+++
@@ -1,5 +0,0 @@
-call mvn clean install
-
-call copy "C:\Dev\project\benevolat-ear\benevolat-root\benevolat-root-ear\target\benevolat-root.ear" "C:\Dev\jboss-as-7.1.1.Final\standalone\deployments"
-
-pause
